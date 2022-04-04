f67c0b74ee07347a9d98a9ca9ecf44f8d4913d5b
DESKTOP-DNP38AM\coffe <coffee_soul@navercom>
2022-04-04 10:56:57 +0900

    ㅅㅈ

--- api/src/main/java/com/whitewise/api/domain/HelloResponseDto.java
+++ api/src/main/java/com/whitewise/api/domain/HelloResponseDto.java
@@ -1,16 +1,30 @@
 package com.whitewise.api.domain;
 
-import lombok.*;
-
-@Getter
-@RequiredArgsConstructor
 public class HelloResponseDto {
 
-    private final String name;
-    private final int amount;
+    private String name;
+    private int amount;
 
     public HelloResponseDto(String name, int amount) {
         this.name = name;
         this.amount = amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public HelloResponseDto setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public HelloResponseDto setAmount(int amount) {
+        this.amount = amount;
+        return this;
     }
 }
