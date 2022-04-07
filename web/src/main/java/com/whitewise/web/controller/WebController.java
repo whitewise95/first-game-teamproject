ee36af3b36b5f58eb0f625cb83b0ead0472242ea
DESKTOP-DNP38AM\coffe <coffee_soul@navercom>
2022-04-07 21:52:37 +0900

    기초

--- web/src/main/java/com/whitewise/web/controller/WebController.java
+++ web/src/main/java/com/whitewise/web/controller/WebController.java
@@ -1,28 +1,11 @@
 package com.whitewise.web.controller;
 
-import com.whitewise.web.domain.posts.Posts;
-import com.whitewise.web.dto.*;
-import com.whitewise.web.model.HelloResponseDto;
-import com.whitewise.web.service.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 
-@RestController("/web")
+@RestController
 @RequiredArgsConstructor
 public class WebController {
 
-    private PostsService postsService;
-
-    @PutMapping("/v1/posts/{id}")
-    public Long update(@PathVariable Long id,
-                       @RequestBody PostsUpdateRequestDto postsUpdateRequestDto) {
-
-        return postsService.update(id, postsUpdateRequestDto);
-    }
-
-    @GetMapping("/v1/posts/{id}")
-    public PostsResponseDto findByid(@PathVariable Long id) {
-        return postsService.findById(id);
-    }
 }
