61cd2de8db94b59cc082bf654586d4b9dae9fb42
whitewise95 <81284265+whitewise95@users.noreply.github.com>
2023-06-11 16:08:35 +0900

    fix  test

--- src/main/java/com/study/spring/components/fireBase/FireBase.java
+++ src/main/java/com/study/spring/components/fireBase/FireBase.java
@@ -6,39 +6,37 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
 import com.study.spring.components.FireBaseProperties;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
-
 import java.io.File;
 import java.io.FileInputStream;
-import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class FireBase {
 
-    private final FireBaseProperties fireBaseProperties;
-    private FileInputStream serviceAccount;
-    private FirebaseOptions options;
+	private final FireBaseProperties fireBaseProperties;
+	private FileInputStream serviceAccount;
+	private FirebaseOptions options;
 
-    private Firestore DB;
+	private Firestore DB;
 
-    public void init() throws Exception {
-        FirebaseApp firebaseApp = null;
-        serviceAccount = new FileInputStream(
-                new File("").getAbsolutePath() + "/" + fireBaseProperties.getFireBaseKey()
-        );
+	public void init() throws Exception {
+		FirebaseApp firebaseApp = null;
+		serviceAccount = new FileInputStream(
+			new File("").getAbsolutePath() + "/" + fireBaseProperties.getFireBaseKey()
+		);
 
-        options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .build();
+		options = new FirebaseOptions.Builder()
+			.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+			.build();
 
-        firebaseApp.initializeApp(options);
+		firebaseApp.initializeApp(options);
 
-        DB = FirestoreClient.getFirestore();
-    }
+		DB = FirestoreClient.getFirestore();
+	}
 
-    public Firestore getDB() {
-        return DB;
-    }
+	public Firestore getDB() {
+		return DB;
+	}
 }
